5fc665e6dcc477e1116efc78656ab6074c3e2be7
Hazel Ng <hazel.ng@mail.utoronto.ca>
2022-03-14 12:48:50 -0400

    edit 4

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,7 +1,7 @@
 module.exports = {
   url: process.env.URL || "https://hackinghistory.github.io/hh-project-11ty-starter-kit/",
   siteName: "Why Classical Music (subject to change)",
-  siteDescription: "A brief history of how the genre Classical Music came to be"
+  siteDescription: "A brief history of how the genre Classical Music came to be",
   authorName: "Hazel Ng",
   twitterUsername: "hazelngsanstwitter", // no `@`
 };
--- src/pages/proposal.md
+++ src/pages/proposal.md
@@ -1,5 +1,5 @@
 ---
-title: "About"
+title: "Proposal"
 description: "A brief introduction to this starter kit."
 ---
 
